b0412fcc3b59a5e539eb4af09f69888b6844ecf1
Dan Black <dblack@diligent.com>
2017-09-18 12:31:37 +1200

    add

--- DevMachineSetup.ps1
+++ DevMachineSetup.ps1
@@ -12,7 +12,11 @@ Param
     $vs,
     
     [Switch]
-    $winSettings
+    $winSettings,
+
+    [Switch]
+    $work
+
 
 )
 
@@ -22,31 +26,47 @@ if( $prepOS )
     Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+if( $work ) 
+{
+    choco install slack -y
+   
+   }
+function ShowFileExtensions{
+    Push-Location
+    Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+    Set-ItemProperty . HideFileExt "0"
+    Pop-Location
+}
+
 if( $winSettings ) {
-    
+    ShowFileExtensions
     choco install taskbar-never-combine -y -force
 }
+
+
 
 if( $tools ) {
     choco install -y googlechrome
     choco install -y 7zip
     choco install -y sysinternals
     choco install -y visualstudiocode
+    choco install -y linqpad4
     
     #git
-    choco install git.install
-    choco install poshgit
-    choco install gitextensions
+    choco install git.install -y
+    choco install poshgit -y
+    choco install gitextensions -y
+    choco install kdiff3 -y
     
-    choco install conemu
+    choco install conemu -y
 
     
    
 }
 
 if( $vs ) {
-    choco install -y visualstudiocode
-    choco install visualstudio2017professional
+    choco install -y visualstudiocode -y
+    choco install visualstudio2017professional -y
+    choco install resharper -y
 }
-
 
